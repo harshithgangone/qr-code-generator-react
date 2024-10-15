d1c6ac7c3a711eb4c92ee7abb66b5b2edc135706
harshithgangone <harshith.gangone@gmail.com>
2024-10-15 12:20:55 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import React, { useState } from 'react'
 const App = () => {
   const [inp,setInput]=useState("")
   const [imageLink,setImageLink]=useState("")
-const GenerateQR=(e)=>{
-    setImageLink(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${e}`)
+const GenerateQR=()=>{
+    setImageLink(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inp}`)
 }
   return (
     <div className='container'>
       <p>Enter your text or URL</p>
-        <input value={inp} onChange={(e)=>GenerateQR(e.target.value)} type="text" placeholder="Text or URL" id="qrText"/>
+        <input value={inp} onChange={(e)=>setInput(e.target.value)} type="text" placeholder="Text or URL" id="qrText"/>
         <div id="imgBox" style={{marginLeft:"90px"}}>
             <img src={imageLink} alt="" id="qrImage"/>
         </div>
